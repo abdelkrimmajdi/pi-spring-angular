bb0fae866e2a40e867507dc6154ef6694a736bb2
abdelkrimmajdi <majdi.abdelkrim@esprit.tn>
2024-02-12 19:20:46 +0100

    VitaVibes

---
+++ Vitanova/src/test/java/com/example/vitanova/VitanovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitanovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
